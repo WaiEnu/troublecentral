e9494b8d7b02c1a4135d7be7832f89044791ff72
WaiEnu <strayhituj@gmail.com>
2020-03-28 14:39:40 +0900

    20.03.28

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ class Dna(Resource):
 api.add_resource(Dna, '/dna')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(host="0.0.0.1", port=5000,debug=True)
